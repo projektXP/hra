231fc89e05f3fae3be03e6e554c8583d2068edd2
Michal Puškel <michal.puskel@ui42.sk>
2016-04-05 11:30:28 +0200

    @1 1 a posledny raz v zivote... blahova podmienka

--- monster.py
+++ monster.py
@@ -1,24 +1,33 @@
 from thing import Thing
-
+from player import Player
 
 class Monster(Thing):
-    def __init__(self, room, x=0, y=0):
+    def __init__(self, room, x=0, y=0, player=None):
         super().__init__(room, x, y)
+
+        self.player = player
 
 class FastMonster(Monster):
-    def __init__(self, room, x=0, y=0):
-        super().__init__(room, x, y)
+    def __init__(self, room, x=0, y=0, player=None):
+        super().__init__(room, x, y, player)
 
         self.fast = True
+        self.track = False
 
     def step(self):
         pass
 
 class SlowMonster(Monster):
-    def __init__(self, room, x=0, y=0):
-        super().__init__(room, x, y)
+    def __init__(self, room, x=0, y=0, player=None):
+        super().__init__(room, x, y, player)
 
         self.fast = False
+        self.track = True
 
     def step(self):
-        pass
+        if self.player and (self.x != self.player.x or self.y != self.player.y):
+            dx = self.player.x - self.x
+            dy = self.player.y - self.y
+            if dx + dy == dx or dx + dy == dy:
+                pass
+            else:
