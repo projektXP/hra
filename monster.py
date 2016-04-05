ba542cebb05cfef2c6d2609b7b49fbb4ae0e18a9
Michal Puškel <michal.puskel@ui42.sk>
2016-04-05 11:45:06 +0200

    @1 SlowMonster step AI

--- monster.py
+++ monster.py
@@ -1,3 +1,5 @@
+import random
+
 from thing import Thing
 from player import Player
 
@@ -9,6 +11,7 @@ class Monster(Thing):
 
 class FastMonster(Monster):
     def __init__(self, room, x=0, y=0, player=None):
+        super().__init__(room, x, y, player)
         super().__init__(room, x, y, player)
 
         self.fast = True
@@ -25,9 +28,16 @@ class SlowMonster(Monster):
         self.track = True
 
     def step(self):
-        if self.player and (self.x != self.player.x or self.y != self.player.y):
+        if self.player:
             dx = self.player.x - self.x
             dy = self.player.y - self.y
-            if dx + dy == dx or dx + dy == dy:
-                pass
-            else:
+            if self.x != self.player.x and self.y == self.player.y:
+                nx = self.x + dx / dx
+            elif self.x == self.player.x and self.y != self.player.y:
+                ny = self.y + dy / dy
+            elif self.x != self.player.x and self.y != self.player.y:
+                if random.randrange(2):
+                    nx = self.x + dx / dx
+                else:
+                    ny = self.y + dy / dy
+        self.move_to(nx, ny)
