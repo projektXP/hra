0d216f7a96f79a294320295d701d11c305f17d5b
Michal Puškel <michal.puskel@ui42.sk>
2016-04-05 11:02:15 +0200

    @1 monster klasy kostra

--- monster.py
+++ monster.py
@@ -2,5 +2,23 @@ from thing import Thing
 
 
 class Monster(Thing):
+    def __init__(self, room, x=0, y=0):
+        super().__init__(room, x, y)
+
+class FastMonster(Monster):
+    def __init__(self, room, x=0, y=0):
+        super().__init__(room, x, y)
+
+        self.fast = True
+
+    def step(self):
+        pass
+
+class SlowMonster(Monster):
+    def __init__(self, room, x=0, y=0):
+        super().__init__(room, x, y)
+
+        self.fast = False
+
     def step(self):
         pass
