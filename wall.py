20cfc2f469bdec06e822758cf69180e8cf182c9f
martinkellner <kellner7@uniba.sk>
2016-04-06 17:33:01 +0200

    Create wall.py

---
+++ wall.py
@@ -0,0 +1,16 @@
+from thing import Thing
+
+class Wall(Thing):
+
+    def __init__(self, room, x = 0, y = 0, image):
+
+        super().__init__(self, room, x = 0, y = 0)
+        self.image = image
+         
+    def move_to():
+        pass
+
+    
+##    def draw(self):           
+##        return self.canvas 
+    
