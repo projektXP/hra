5ee7764984b36763b628c1d00514068ebeeffddf
martinkellner <martin.kellner.1@gmail.com>
2016-04-13 18:13:17 +0200

    pictures added

--- wall.py
+++ wall.py
@@ -3,13 +3,11 @@ from thing import Thing
 
 class Wall(Thing):
     def __init__(self, room, x, y, nameOfPic='wall'):
-
         super().__init__(room,x,y)
         self._address = self._retPicture(nameOfPic)
         self.drawPic()
 
     def _retPicture(self,name) -> str:
-
         if name in self.dictOfPic:
             return self.dictOfPic[name]
         return ""
